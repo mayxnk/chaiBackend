4520f6cfdb5ac4b50ca7e41cabf29467766f4405
Mayank Rastogi <rastogimayank45@gmail.com>
2024-03-24 01:45:58 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+const express = require("express");
+
+const app = express();
+
+const port = process.env.PORT;
+
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+
+app.get('/twitter', (req, res) => {
+    res.send('@mayank__rastogi')
+})
+
+app.get('/login', (req, res) => {
+    res.send('<h1>Please login at Chai aur Code</h1>')
+})
+
+app.get('/youtube', (req, res) => {
+    res.send('<h2>Chai aur Code</h2>')
+})
+
+app.listen(process.env.PORT, () => {
+  console.log(`Example app listening on port ${port}`);
+});
